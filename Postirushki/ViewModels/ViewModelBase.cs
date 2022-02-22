f85e1d6724446a86af4f8cce45cd232e7f3a8443
Skiexx <krupashow@ya.ru>
2022-02-22 19:01:04 +0700

    Create

---
+++ Postirushki/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Postirushki.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Postirushki/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Postirushki.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
